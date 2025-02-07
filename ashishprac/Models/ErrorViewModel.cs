ccbeaec6ef96a44d5a752e3b07355a0182974d66
849ashish <65883806+849ashish@users.noreply.github.com>
2025-02-07 16:29:26 +0530

    Delete ashishprac directory

--- ashishprac/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ashishprac.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
